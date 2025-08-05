8a6efb29f77d043dc7744881f9dcbd1cf5f0e030
Clayton Oliveira <clayton.will08@gmail.com>
2025-08-04 21:54:04 -0300

    - Add metodo create - Add metodo edit - Add metodo edit

--- SaphiraTerror/Controllers/UsuarioController.cs
+++ SaphiraTerror/Controllers/UsuarioController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SaphiraTerror.Interfaces;
+using SaphiraTerror.Models;
 using SaphiraTerror.ViewModels;
 using System.Security.Claims;
 
@@ -109,6 +111,119 @@ namespace SaphiraTerror.Controllers
                     Text = t.DescricaoTipoUsuario
                 })
             };
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UsuarioViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = new Usuario
+                {
+                    Nome = viewModel.Nome,
+                    Email = viewModel.Email,
+                    Senha = viewModel.Senha,
+                    DataNascimento = viewModel.DataNascimento,
+                    TipoUsuarioId = viewModel.TipoUsuarioId,
+                    Ativo = true
+                };
+                await _usuarioRepository.AddAsync(usuario);
+                return RedirectToAction(nameof(Index));
+            }
+            viewModel = await CriarUsuarioViewModel();
+            return View(viewModel);
+        }
+
+        //edit 
+        [Authorize(Roles = "Administrator, Gerente")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var usuario = await _usuarioRepository.GetByIdAsync(id);
+            if (usuario == null)
+                return NotFound();
+
+            var viewModel = new UsuarioViewModel
+            {
+                IdUsuario = usuario.IdUsuario,
+                Nome = usuario.Nome,
+                Email = usuario.Email,
+                DataNascimento = usuario.DataNascimento,
+                TipoUsuarioId = usuario.TipoUsuarioId,
+                TiposUsuario = (await _tipoUsuarioRepository.GetAllAsync()).Select(t => new SelectListItem
+                {
+                    Value = t.IdTipoUsuario.ToString(),
+                    Text = t.DescricaoTipoUsuario,
+                })
+            };
+            return View(viewModel);
+        }
+
+        //edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, UsuarioViewModel viewModel)
+        {
+            if (id != viewModel.IdUsuario)
+                return NotFound("Não encontrado!");
+
+            if (ModelState.IsValid)
+            {
+                var usuario = await _usuarioRepository.GetByIdAsync (id);
+                if(usuario == null) 
+                    return NotFound();
+                usuario.Nome = viewModel.Nome;
+                usuario.Email = viewModel.Email;
+                usuario.Senha = viewModel.Senha;
+                usuario.DataNascimento = viewModel.DataNascimento;
+                usuario.TipoUsuarioId = viewModel.TipoUsuarioId;
+                await _usuarioRepository.UpdateAsync(usuario);
+                return RedirectToAction(nameof(Index));
+            }
+
+            viewModel = await CriarUsuarioViewModel(viewModel);
+            return View(viewModel);
+        }
+
+        //delete
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var usuario = await _usuarioRepository.GetByIdAsync(id);
+
+            return View(usuario);
+        }
+
+        //softdelete
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> SoftDelete(int id)
+        {
+            await _usuarioRepository.InativarAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //listar inativos
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> GetAllInativos()
+        {
+            var usuarios = await _usuarioRepository.GetAllAsync();
+            var inativos = usuarios.Where(u => !u.Ativo).OrderByDescending(u => u.IdUsuario).ToList();
+
+            return View(inativos);
+        }
+
+        //ativar usuario
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> AtivarUsuario(int id)
+        {
+            var usuario = await _usuarioRepository.GetByIdAsync(id);
+            if (usuario == null)
+                return NotFound();
+
+            usuario.Ativo = true;
+            await _usuarioRepository.UpdateAsync(usuario);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
