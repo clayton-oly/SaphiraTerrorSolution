599738a47bfe62731e98109af1a9ce639f706f55
Clayton Oliveira <clayton.will08@gmail.com>
2025-09-16 22:06:10 -0300

    Add filtro por id genero com jquery Add lista de filmes dentro dos cards

--- SaphiraTerror/wwwroot/js/site.js
+++ SaphiraTerror/wwwroot/js/site.js
@@ -35,4 +35,64 @@ $(document).ready(function () {
             console.log('Deu erro!');
         }
     });
+
+    //Ao clicar em um dos generos (elemento button dentro da div id genre)
+    $(document).on('click', '#genre button', function () {
+        //pegar o valor do atributo 'data-genero do botão clicado
+        const generoEscolhido = $(this).data('genero');
+        console.log('Gênero:', generoEscolhido);
+
+        //chamar a função buscarFilmes, passando o genero
+        buscarFilmes(generoEscolhido);
+
+    });
+
+    buscarFilmes("todos");
+
+    function buscarFilmes(generoEscolhido) {
+        url = "/filme";
+        if (generoEscolhido != 'todos') {
+            url = url + "/" + generoEscolhido;
+        }
+        $.ajax({
+            url: urlBase + url,
+            type: "GET",
+            contentType: "application/json",
+            //se der sucesso (200) cai aqui nesse bloco
+            success: function (dados) {
+
+                //selecionar a div dos filmes
+                const divFilmes = $('#filmes-container');
+                divFilmes.html("");
+
+                dados.forEach(filme => {
+
+                    //cor do selo de classificação
+                    corBagde = "success";
+                    if (filme.classificacao == "+18") {
+                        corBagde = "danger";
+                    } else if (filme.classificacao == "Kids") {
+                        corBagde = "primary";
+                    }
+
+                    const card = `<div class="col">
+                    <div class="card bg-dark border-secondary h-100">
+                        <img src="${filme.urlImagem}" class="card-img-top" alt="${filme.titulo}" style="height: 250px; object-fit: cover;">
+                        <div class="card-body">
+                            <h5 class="card-title">${filme.titulo}</h5>
+                            <p class="card-text text-muted">${filme.genero}</p>
+                            <p class="card-text small text-muted">${filme.produtora}</p>
+                            <span class="badge bg-${corBagde}">${filme.classificacao}</span>
+                        </div>
+                    </div>
+                </div>`;
+
+                    divFilmes.append(card);
+                });
+            },
+            error: function (erro) {
+                console.log('Erro ao carrregar filmes!');
+            }
+        });
+    }
 });
