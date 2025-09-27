03c8cf946db738d3b06355d8cd7ad86fc32a3d6f
Clayton Oliveira <clayton.will08@gmail.com>
2025-09-26 21:24:39 -0300

    Deploy

--- SaphiraTerror.API/Program.cs
+++ SaphiraTerror.API/Program.cs
@@ -29,17 +29,15 @@ builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 //ativar cors
 app.UseCors();
